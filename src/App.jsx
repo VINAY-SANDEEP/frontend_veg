c36cb7300f43d1abd43b2a547c69d68ab3c95683
VINAY-SANDEEP <rsrinivasa782@gmail.com>
2025-07-06 17:33:21 +0530

    JJJ

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,15 @@ import Orders from './components/Orders';
 import Customers from './components/Customers';
 import Layout from './components/Layout';
 import Addproducts from './components/Addproducts';
+import Navbar3 from './components/NavBar3';
 
 export default function App() {
   return (
     <Router>
       <Routes>
+
         <Route path="/login" element={<Login />} />
+        <Route path="/Nav" element={<Navbar3 />} />
         <Route path="/register" element={<Register />} />
         <Route path="/" element={<Layout><Dashboard /></Layout>} />
         <Route path="/products" element={<Layout><Products /></Layout>} />
