2f39e36c6d279883b7c27d9fd43b4c6efa365787
VINAY-SANDEEP <rsrinivasa782@gmail.com>
2025-07-06 17:11:33 +0530

    jj

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/login';
 import Register from './components/Register';
 import Dashboard from './components/Dashboard';
@@ -9,13 +9,12 @@ import Layout from './components/Layout';
 import Addproducts from './components/Addproducts';
 
 export default function App() {
-  const isLoggedIn = !!localStorage.getItem('token');
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/" element={isLoggedIn ? <Layout><Dashboard /></Layout> : <Navigate to="/login" />} />
+        <Route path="/" element={<Layout><Dashboard /></Layout>} />
         <Route path="/products" element={<Layout><Products /></Layout>} />
         <Route path="/Addproducts" element={<Layout><Addproducts /></Layout>} />
         <Route path="/orders" element={<Layout><Orders /></Layout>} />
